9b6bc3d10683f9fed2a2ff6631d301d408975dc7
Roop Kumar Lala <ui20cs60@iiitsurat.ac.in>
2023-12-20 13:44:32 +0530

    Game Logic

--- SnakeLadder/src/App.java
+++ SnakeLadder/src/App.java
@@ -14,4 +14,29 @@ class Player{
         Random random = new Random();
         return random.nextInt(1,6);
     }
+
+    void play(){
+        // OPTIONS:
+        // 1. No Play
+        // 2. Ladder
+        // 3. Snake
+        
+        Random random = new Random();
+        int option = random.nextInt(1,3);
+        int move = rollDice();
+
+        switch (option) {
+            case 1:
+                position += 0;
+                break;
+            
+            case 2:
+                position += move;
+                break;
+            
+            case 3:
+                position -= move;
+                break;
+        }
+    }
 }
